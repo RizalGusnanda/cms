51179888341029d94606d1a5d3047a5e238ea418
= <=>
2022-02-23 17:58:21 +0700

    praktikum 3

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,17 +4,9 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
-        echo "Hi! Selamat Datang di Website Laravel";
-    }
+    
 
-    public  function about(){
-        echo "Nama  : Muchamad Rizal Gusnanda Atmaja <br>";       
-        echo "Nim   : 2041720015 <br>";
-        echo "Kelas : TI 2G";
-    }
+    
 
-    public function articles($id){
-        echo "Ini adalah halaman Artikel dengan id " . $id;
-    }
+    
 }
