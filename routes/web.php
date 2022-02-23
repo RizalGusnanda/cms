1c60eaa7a0166185b0bd61a8c3106619e20e2834
= <=>
2022-02-23 17:55:59 +0700

    praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,54 @@ use App\Http\Controllers\ArticleController;
 // Route::get('/articles/{id}', [PageController::class, 'articles']);
 
 //nomer 5
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [AboutController::class, 'about']);
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get('/about', [AboutController::class, 'about']);
+// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+//Praktilkum 3
+// Route::get('/',[prak3Controller::class,'index']);
+Route::get('/', function () {
+    echo "Selamat Datang Di Home Company";
+});
+
+
+Route::prefix('product') -> group(function(){
+    Route::get("", function(){
+        echo "Menampilkan macam-macam product<br><br>";
+        
+        echo "marbel-and-friends-kids-games<br>";
+        echo "riri-story-books<br>";
+        echo "kolak-kids-songs<br>";
+        echo "marbel-edu-games";
+    });
+});
+
+Route::prefix('news') -> group(function(){
+
+    Route::get("", function(){
+        echo "Menampilkan daftar berita<br><br>";
+
+        echo "educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19<br>";
+        echo "educa-studio-penggalangan-dana-untuk-bencana-banjir";
+    });
+    
+});
+
+Route::prefix('program') -> group(function(){
+
+    Route::get("", function(){
+        echo "Menampilkan daftar program<br><br>";
+
+        echo "program/karir<br>";
+        echo "program/magang<br>";
+        echo "program/kunjungan-industri<br>";
+    });
+    
+});
+
+
+Route::get("/about-us" , function(){
+    echo " Nama  : Muchamad Rizal Gusnanda Atmaja<br>";
+    echo " Nim   : 2041720015<br>";
+    echo " Kelas : TI 2G";
+});
