21c53b8022fcfb909cc6c130cfc9c30ce9507ec5
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2022-05-25 12:58:41 +0700

    fix config

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,5 +28,6 @@ class HomeController extends Controller
     {
         $user = Auth::user();
         return view('home', ['user' => $user]);
+        // return view('home', ['user' => $user]);
     }
 }
